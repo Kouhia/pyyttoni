dad1709e70faa2351f5b964e9eea4a1c8ac82416
Kouhia <herra.kouhia@gmail.com>
2016-05-03 12:46:07 +0300

    Simple TCP socket example.

--- TCPtest.py
+++ TCPtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Simple TCP test
+# Simple TCP test example
 
 import socket
 
@@ -8,11 +8,11 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5055
 BUFFER_SIZE = 1024
-MESSAGE="Hello, World!"
+MESSAGE="Hello, TCP socket!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((TCP_IP, TCP_PORT))
+socket.send(MESSAGE)
+data = socket.recv(BUFFER_SIZE)
 s.close()
 
